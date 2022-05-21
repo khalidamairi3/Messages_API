7e1a6add76c2332b3dad84bf474e4276e5eb2971
khalidamairi3 <khalidamairi3@gmail.com>
2022-05-20 23:05:46 -0600

    make calls

--- src/main/java/com/example/chat/controllers/MessagingController.java
+++ src/main/java/com/example/chat/controllers/MessagingController.java
@@ -2,19 +2,22 @@ package com.example.chat.controllers;
 
 
 import com.example.chat.dto.MessageRequest;
+import com.example.chat.services.CallService;
 import com.example.chat.services.SMSService;
+import com.example.chat.services.TwilioSmsSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("messages")
 public class MessagingController {
     private final SMSService service;
-
+    @Autowired
+    private CallService callService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessagingController.class);
     @Autowired
     public MessagingController(SMSService service) {
         this.service = service;
@@ -29,11 +32,14 @@ public class MessagingController {
     public ResponseEntity sendMessage(@RequestBody MessageRequest messageRequest){
         try {
             service.sendSms(messageRequest);
+            LOGGER.info("sms sent successfully");
             return ResponseEntity.accepted().build();
         }catch (Exception ex){
             ex.printStackTrace();
+            LOGGER.error("sms failed");
             return ResponseEntity.badRequest().build();
         }
 
     }
+
 }
